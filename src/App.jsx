0842fe72b2f2fecee58ea49424dceaed115da4d1
brunomachadors <brunomachadors@gmail.com>
2023-10-24 14:51:10 +0100

    Using the rick and morty api to search

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
+import { useEffect, useState } from 'react';
+import { debounce } from 'lodash';
+import { getAllCharacters, searchCharacterName } from './api/character';
+import './style.css';
+
 function App() {
+  const [characters, setCharacters] = useState({});
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const getInfo = async () => {
+      const newCharacters = await getAllCharacters();
+      setCharacters(newCharacters);
+    };
+
+    getInfo();
+  }, []);
+
+  const debouncedSearch = debounce(async (queryText) => {
+    const newResults = await searchCharacterName(queryText);
+    setResults(newResults);
+  }, 2000);
+
+  const handleSearch = async (event) => {
+    setQuery(event.target.value);
+    debouncedSearch(query);
+  };
 
   return (
     <>
+      <p>Number of characters: {characters.count}</p>
+      <label htmlFor="search">Search characters: </label>
+      <input
+        type="text"
+        id="search"
+        value={query}
+        onChange={handleSearch}
+      ></input>
+      <ul>
+        {results &&
+          results.map((item) => (
+            <li key={item.id} className="search-result">
+              <h3>{item.name}</h3>
+              <img src={item.image} />
+              <button>Add to favorites</button>
+            </li>
+          ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/api/character/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export async function getAllCharacters() {
+  const response = await axios.get('https://rickandmortyapi.com/api/character');
+  console.log(response.data);
+  return response.data.info;
+}
+
+export async function searchCharacterName(characterName) {
+  try {
+    const response = await axios.get(
+      `https://rickandmortyapi.com/api/character/?name=${characterName}`
+    );
+    const characters = response.data.results.map((item) => ({
+      id: item.id,
+      name: item.name,
+      image: item.image,
+    }));
+    return characters;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
